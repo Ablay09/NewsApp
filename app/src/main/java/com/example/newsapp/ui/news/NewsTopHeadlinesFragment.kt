980579d98710706f435b7672c4138963d1e710da
ablay <ayedilbayev@one.kz>
2021-03-13 14:11:20 +0600

    feat: add topHeadline news

--- app/src/main/java/com/example/newsapp/ui/news/NewsTopHeadlinesFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/news/NewsTopHeadlinesFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.newsapp.R
 import com.example.newsapp.core.extensions.getErrorMessage
 import com.example.newsapp.core.extensions.showToast
+import com.example.newsapp.ui.news.adapter.NewsListAdapter
+import kotlinx.android.synthetic.main.fragment_news_top_headlines.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class NewsTopHeadlinesFragment : Fragment() {
 
     private val viewModel: NewsViewModel by viewModel()
+    private val adapter: NewsListAdapter by lazy {
+        NewsListAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,14 +32,22 @@ class NewsTopHeadlinesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.fetchTopHeadlineNews()
+        initRecyclerView()
         observeViewModel()
+    }
+
+    private fun initRecyclerView() {
+        rvTopHeadlineNews.adapter = adapter
     }
 
     private fun observeViewModel() {
         viewModel.topHeadlinesLiveData.observe(viewLifecycleOwner) {
             it
                 .onFailure { throwable -> showToastMessage(requireContext().getErrorMessage(throwable)) }
-                .onSuccess { result -> Log.d(this::class.simpleName, "$result") }
+                .onSuccess { result ->
+                    adapter.submitList(result)
+                    Log.d(this::class.simpleName, "$result")
+                }
         }
     }
 
